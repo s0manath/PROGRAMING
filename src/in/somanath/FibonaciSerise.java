f6ebdd9ba8cd58ba7f16167ab4d768478fe04a50
s0manath <soma756126@gmail.com>
2024-04-09 12:16:31 +0530

    Fibonaci serise

---
+++ src/in/somanath/FibonaciSerise.java
@@ -0,0 +1,22 @@
+package in.somanath;
+
+import java.util.Scanner;
+
+public class FibonaciSerise {
+	public static void main(String[] args) {
+		try (Scanner s = new Scanner(System.in)) {
+			System.out.println("Enter The Number");
+			int x=s.nextInt();
+			int first =0;
+			int second=1;
+			for(int i=1;i<=x;i++)
+			{
+				System.out.println(first+" ");
+				int temp=first+second;
+				first=second;
+				second=temp;
+			}
+		}
+	}
+
+}
