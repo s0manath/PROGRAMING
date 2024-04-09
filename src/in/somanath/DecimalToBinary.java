5951012ac0dbd5e1e359cb9d18f1893ab8ba57f1
s0manath <soma756126@gmail.com>
2024-04-09 10:59:45 +0530

    DecimalToBinary

---
+++ src/in/somanath/DecimalToBinary.java
@@ -0,0 +1,27 @@
+package in.somanath;
+
+import java.util.Scanner;
+
+public class DecimalToBinary {
+	public static void main(String[] args) {
+		try (Scanner s = new Scanner(System.in)) {
+			System.out.println("Enter the Number");
+			int x=s.nextInt();
+			String st=DeciToBin(x);
+			System.out.println(st);
+		}
+	}
+
+	private static String DeciToBin(int x) {
+		
+		String s="";
+		while(x!=0) {
+			int r=x%2;
+			s=r+s;
+			x=x/2;
+		}
+		
+		return s;
+	}
+
+}
---
+++ src/in/somanath/GreaterAndSmallerDigit.java
@@ -0,0 +1,40 @@
+package in.somanath;
+
+import java.util.Scanner;
+
+public class GreaterAndSmallerDigit {
+	public static void main(String[] args) {
+		try (Scanner s = new Scanner(System.in)) {
+			System.err.println("Enter the Number");
+			int x=s.nextInt();
+			int greater=greaterDigit(x);
+			int smaller=smallerDigit(x);
+			System.out.println(greater+" is greater Digit");
+			System.out.println(smaller+" is Smaller Digit");
+		}
+	}
+
+	private static int smallerDigit(int x) {
+		
+		int small=9;
+		while(x!=0) {
+			int r=x%10;
+			if(r<small)
+				small=r;
+			x=x/10;
+		}
+		return small;
+	}
+
+	private static int greaterDigit(int x) {
+		int big=-9;
+		while(x!=0) {
+			int r=x%10;
+			if(r>big)
+				big=r;
+			x=x/10;
+		}
+		return big;
+	}
+
+}
